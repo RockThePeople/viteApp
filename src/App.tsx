4bc52140c55023d0f460acc08adef34cd054f297
박민석 <3qufqks@inu.ac.kr>
2025-02-26 04:54:00 +0000

    name input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState({
+    name:""
+  });
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
+    alert(formData.name);
+  }
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
 
   return (
     <>
@@ -21,15 +34,30 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <form onSubmit={handleSubmit}>
+          <label className="block text-gray-600 mb-1" htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
+          />
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+          <button type='submit'>Submit</button>
+        </form>
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
+
 }
 
 export default App
